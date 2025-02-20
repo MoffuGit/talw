95cdfd57bd2d0f5ffea7271ac6292a09d440ca45
Adrian Hess <moffu.by@gmail.com>
2025-02-20 16:24:12 -0600

    update to tailwind 0.4

--- src/app/components/thread/sidebar/mod.rs
+++ src/app/components/thread/sidebar/mod.rs
@@ -37,10 +37,10 @@ pub fn ThreadSideBar() -> impl IntoView {
                                     view! {
                                         <div class=" flex flex-col flex-1">
                                             <ThreadHeader />
-                                            <div class="flex-grow overflow-auto bg-base-200" />
-                                            <div class="h-20 flex-shrink-0 flex">
+                                            <div class="grow overflow-auto bg-base-200" />
+                                            <div class="h-20 shrink-0 flex">
                                                 // NOTE: move this to his own component,
-                                                <div class="m-4 w-full flex-grow bg-base-300/60 rounded-lg flex items-center px-4">
+                                                <div class="m-4 w-full grow bg-base-300/60 rounded-lg flex items-center px-4">
                                                     <Icon
                                                         icon=icondata::RiAddCircleSystemFill
                                                         // class="w-7 h-7 fill-base-content/40 grow-0 mr-4"
--- src/app/routes/servers/thread/mod.rs
+++ src/app/routes/servers/thread/mod.rs
@@ -57,9 +57,9 @@ pub fn Thread() -> impl IntoView {
                                                         // this is the future chat
                                                         // NOTE: move this to his own component,
                                                         <div class="flex flex-col h-auto w-full">
-                                                            <div class="flex-grow overflow-auto" />
-                                                            <div class="h-20 flex-shrink-0 flex">
-                                                                <div class="m-4 w-full flex-grow bg-base-300/60 rounded-lg flex items-center px-4">
+                                                            <div class="grow overflow-auto" />
+                                                            <div class="h-20 shrink-0 flex">
+                                                                <div class="m-4 w-full grow bg-base-300/60 rounded-lg flex items-center px-4">
                                                                     <Icon
                                                                         icon=icondata::RiAddCircleSystemFill
                                                                         // class="w-7 h-7 fill-base-content/40 grow-0 mr-4"

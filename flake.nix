76b991fac5be62fb1a95db581c09169518f43321
Adrian Hess <home@BackUps-Mac-mini.local>
2025-04-14 22:04:40 -0600

    added flakes

--- flake.nix
+++ flake.nix
@@ -42,8 +42,28 @@
                 }
               ))
             ];
+	
+	 shellHook = ''
+MYSQL_HOME="$PWD/mysql"
+export MYSQL_SOCKET="$MYSQL_HOME/mysql.sock"
 
-          shellHook = ''
+if [ ! -d "$MYSQL_HOME/mysql" ]; then
+  echo "Initializing MySQL data directory..."
+  mysqld --defaults-file=/dev/null --initialize --datadir="$MYSQL_HOME" --basedir=$(dirname $(dirname $(which mysqld)))
+fi
+
+if ! pgrep -x "mysqld" > /dev/null; then
+    nohup mysqld --defaults-file=/dev/null --datadir=$MYSQL_HOME --basedir=$(dirname $(dirname $(which mysqld))) --socket=$MYSQL_SOCKET --pid-file=$MYSQL_HOME/mysql.pid --disable-mysqlx > /dev/null 2>&1 &
+fi
+
+shutdown_mysql() {
+  if [ -f "$MYSQL_HOME/mysql.pid" ]; then
+    pkill -u "$USER" mysqld &> /dev/null
+    rm "$MYSQL_HOME/mysql.pid"
+  fi
+}
+
+trap shutdown_mysql EXIT
           '';
         };
       }
